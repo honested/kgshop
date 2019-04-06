e1a88cd2d12c851c876d76b66ef02e54c52a3506
honested <honested@naver.com>
2019-04-06 15:29:39 +0900

    19.04.06 update

--- js/index.js
+++ js/index.js
@@ -2,4 +2,22 @@ $(document).ready(function(){
     $(".ban_top span").click(function(){
         $(".wrap").animate({top:0});
     });
+    $('.main_img').slick({
+        dots: false,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear',
+        autoplay: true,
+        autoplaySpeed: 3000,
+        arrows: false
+    });
+    $('.slider').slick({
+        dots: true,
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        arrows: true
+    });
 });
+
